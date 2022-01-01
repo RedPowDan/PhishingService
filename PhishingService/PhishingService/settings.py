05d572a6d323fda8ec89cdd2aef2bae845715ed2
Danil <11051969dahul@mail.ru>
2022-01-01 22:31:31 +0500

    Миграции + добавил секретный ключ в env

--- PhishingService/PhishingService/settings.py
+++ PhishingService/PhishingService/settings.py
@@ -23,7 +23,7 @@ sys.path.insert(0, os.path.join(PROJECT_ROOT, 'apps'))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '59-&b_)iznqcw(u_6#jd%c4jzz2x6gp%77zrz2xhg$=0i)!f2o'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
